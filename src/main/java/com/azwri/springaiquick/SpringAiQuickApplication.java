ff95bf0f4095f1ab0c3ee8b04b3eae414b8cdc03
azwri <aazwri@gmail.com>
2024-11-27 13:26:06 +0300

    initial the project

---
+++ src/main/java/com/azwri/springaiquick/SpringAiQuickApplication.java
@@ -0,0 +1,13 @@
+package com.azwri.springaiquick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiQuickApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiQuickApplication.class, args);
+    }
+
+}
